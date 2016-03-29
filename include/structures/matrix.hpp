282738195c2da10aab0fa4b8ddc3d136e0d4bd9a
Matteus Legat <matteuslegat@gmail.com>
2016-03-29 17:26:56 -0300

    matrix structure separated in header and source

--- include/structures/matrix.hpp
+++ include/structures/matrix.hpp
@@ -2,91 +2,23 @@
 #define GTKMM_APP_MATRIX
 
 template <typename T>
-
 class Matrix
 {
 
 public:
 
-  Matrix(int rows, int columns)
-      : rows(rows),
-        columns(columns)
-  {
-    matrix = new T*[rows];
-    for(int i = 0; i < rows; i++)
-    {
-      matrix[i] = new int[columns];
-    }
-    for(int i = 0; i < rows; i++)
-    for (int j = 0; j < columns; j++)
-    {
-      matrix[i][j] = 0;
-    }
-  }
+  Matrix(int rows, int columns);
+  ~Matrix();
 
-  int get(int row, int column)
-  {
-    if (column >= this->columns || row >= this->rows)
-    {
-      throw 11;
-    }
-    else
-    {
-      return this->matrix[row][column];
-    }
-  }
-  ~Matrix() {}
+  void set(int row, int column, T value);
+  int get(int row, int column);
+  int getRows();
+  int getColumns();
 
-  void set(int row, int column, T value)
-  {
-    if (column >= this->columns || row >= this->rows)
-    {
-      return;
-    }
-    else
-    {
-      this->matrix[row][column] = value;
-    }
-  }
+  Matrix operator+(Matrix another);
+  Matrix operator*(Matrix<T> another);
 
-  Matrix operator+(Matrix another)
-  {
-    if (this->columns != another.columns || this->rows != another.rows)
-    {
-      throw 20;
-    }
-    else
-    {
-      Matrix sum = Matrix<T>(rows, columns);
-      for (int i = 0; i < rows; i++)
-      for (int j = 0; j < columns; j++)
-      {
-        sum.set(i, j, this->get(i,j) + another.get(i,j));
-      }
-      return sum;
-    }
-  }
-
-  Matrix operator*(Matrix<T> another)
-  {
-    if (this->columns != another.rows)
-    {
-      throw 21;
-    }
-    else
-    {
-      Matrix mult = Matrix(this->rows, another.columns);
-      for (int i = 0; i < this->rows; i++)
-      for (int j = 0; j < another.columns; j++)
-      for (int k = 0; k < another.rows; k++)
-      {
-        mult.matrix[i][j] += this->matrix[i][k] * another.matrix[k][j];
-      }
-      return mult;
-    }
-  }
-
-// protected:
+protected:
   int rows, columns;
   T** matrix;
 
