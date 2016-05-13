396fa81fa59301b8f24278c852c58179a6722db3
Matteus Legat <matteuslegat@gmail.com>
2016-05-12 23:48:15 -0300

    moodle join script improved

--- moodle/moodlefy.sh
+++ moodle/moodlefy.sh
@@ -3,6 +3,7 @@
 upfolder=UPLOAD_THIS_FOLDER
 source='../src/elements/*.cpp ../src/view/*.cpp ../src/services/*.cpp ../src/structures/*.cpp ../main.cpp'
 headers='../include/elements/*.hpp ../include/services/*.hpp ../include/structures/*.hpp ../include/view/*.hpp ../include/enumerations/*.hpp'
+alert="// AVISO: COMO O LIMITE DE ARQUIVOS FOI EXCEDIDO, TODOS OS ARQUIVOS .CPP FORAM\n// UNIDOS EM UM ÚNICO CPP, E PARA FACILITAR A AVALIAÇÃO, TAMBÉM FOI ENVIADO UM\n// ARQUIVO .ZIP COM TODOS OS ARQUIVOS SEPARADOS."
 
 # remove folder and its contents if exists
 [ -e $upfolder ] && rm -r ./UPLOAD_THIS_FOLDER
@@ -11,19 +12,25 @@ headers='../include/elements/*.hpp ../include/services/*.hpp ../include/structur
 mkdir $upfolder
 
 # copy static files
-cp ./static/moodle_compile.sh $upfolder/compile.sh
-cp ./static/moodle_readme.txt $upfolder/README.txt
-cp ../LICENSE $upfolder/LICENSE
-cp ../test.obj $upfolder/test.obj
+cp ./static/compile.sh $upfolder/
+cp ./static/README.txt $upfolder/
+cp ../test.obj $upfolder/
+cp ../cube.obj $upfolder/
 
 
 # copy headers
 cp $headers -t $upfolder
 
+# if parameter `join` was passed:
 if [ "$1" == "join" ]; then
-  # join cpp files if parameter `join` was passed
-  find $source -type f -printf '\n\n// ========== FILE: %p ========== //\n\n\n'\
-      -exec cat {} \; > $upfolder/source.cpp
+  echo -e $alert > $upfolder/source.cpp
+  # join cpp files:
+  find $source -type f -printf '\n\n// ========== FILE: %p ========== //\n\n\n' -exec cat {} \; >> $upfolder/source.cpp
+  # join files in zip:
+  mkdir zip/
+  cp $source $headers ../test.obj ../cube.obj ./static/README.txt ./static/compile.sh -t zip
+  zip -r $upfolder/code.zip zip/ >> /dev/null
+  rm -r zip/
 else
   # copy cpp files
   cp $source -t $upfolder
--- moodle/static/README.txt
+++ moodle/static/README.txt
@@ -1,5 +1,9 @@
 ALUNOS: José Victor Feijó (14100842) e Matteus Legat (14100861)
 
+AVISO: COMO O LIMITE DE ARQUIVOS FOI EXCEDIDO, TODOS OS ARQUIVOS .CPP FORAM
+UNIDOS EM UM ÚNICO CPP, E PARA FACILITAR A AVALIAÇÃO, TAMBÉM FOI ENVIADO UM
+ARQUIVO .ZIP COM TODOS OS ARQUIVOS SEPARADOS.
+
 O trabalho foi feito utilizando a bibilioteca gtkmm 3.0, pois acreditamos que é 
 melhor para o conceito de orientação a objetos também para manutenção e futuras
 atualizações no software que estamos desenvolvendo na disciplina.
@@ -11,9 +15,3 @@ Para compilar e rodar a aplicação, existe também a opção de rodar o arquivo
 chamado "compile.sh", utilizando o comando "bash compile.sh run". Caso queira
 apenas compilar, utilize somente "bash COMPILE.sh". Ao rodar o script, o 
 programa compilado é salvo como app.o.
-
-Em caso de algum problema com biblioteca, é recomendado certificar-se de que a
-biblioteca gtkmm-dev está instalada. No ubuntu, é possível instalá-la com o 
-comando:
-sudo apt-get install libgtkmm-3.0-dev
-
